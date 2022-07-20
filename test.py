0a40fa4071920b29f8162c883b1dbb183358b080
Taro Kimura <tarokimuraorg@outlook.com>
2022-07-20 11:55:55 +0900

    一行の長いコードを複数行に分割

--- test.py
+++ test.py
@@ -10,10 +10,15 @@ with open('./csv/jigyosyo.csv', 'r', encoding='utf8') as csv_file:
     for cnt in range(0,10):
 
         corporate_name = csv_list[cnt][2]
+        corporate_name = corporate_name.strip()
         corporate_name = corporate_name.replace('\u3000', ' ')
         corporate_name = corporate_name.replace('（株）', '株式会社')
 
-        corporate_address = csv_list[cnt][3] + ' ' + csv_list[cnt][4] + ' ' + csv_list[cnt][5] + ' ' + csv_list[cnt][6]
+        corporate_address = csv_list[cnt][3] + ' '
+        corporate_address = corporate_address + csv_list[cnt][4] + ' '
+        corporate_address = corporate_address + csv_list[cnt][5] + ' '
+        corporate_address = corporate_address + csv_list[cnt][6]
+        corporate_address = corporate_address.strip()
 
         print('企業名 : {}'.format(corporate_name))
         print('住所 : {}'.format(corporate_address))
