fdcd354da10e93d0e0ead936abf7784b714a7277
Taro Kimura <tarokimuraorg@outlook.com>
2022-08-01 14:13:12 +0900

    軽微な変更

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ with open('./csv/jigyosyo.csv', 'r', encoding='utf8') as csv_file:
 
     csv_data = csv.reader(csv_file, delimiter=',', doublequote=True, lineterminator='\n', quotechar='"', skipinitialspace=True)
     csv_list = list(csv_data)
+    address_book = []
 
     #print('件数 : {}'.format(len(csv_list)))
 
-    for cnt in range(10,20):
+    for cnt in range(30,40):
 
         corporate_name = csv_list[cnt][2]
         corporate_name = corporate_name.replace('\u3000', ' ')
@@ -20,7 +21,8 @@ with open('./csv/jigyosyo.csv', 'r', encoding='utf8') as csv_file:
         corporate_address = corporate_address + csv_list[cnt][6]
         corporate_address = corporate_address.strip()
 
+        print('{} : {}'.format(cnt, csv_list[cnt]))
+        print('')
         print('企業名 : {}'.format(corporate_name))
         print('住所 : {}'.format(corporate_address))
-        print(csv_list[cnt])
         print('')
