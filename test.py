aeecdab812f7a537fb16a52bf5dcb80df45fde2b
Taro Kimura <tarokimuraorg@outlook.com>
2022-09-07 14:28:59 +0900

    出力結果のチェック中

--- test.py
+++ test.py
@@ -11,18 +11,22 @@ with open('./csv/jigyosyo.csv', 'r', encoding='utf8') as csv_file:
 
     #print('件数 : {}'.format(len(csv_list)))
 
-    for cnt in range(0,10):
+    for cnt in range(62,72):
 
         print('{} : {}'.format(cnt, csv_list[cnt]))
         print('')
-        print('企業名 : {}'.format(test_address_book[cnt].name))
-        print('住所 : {}'.format(test_address_book[cnt].address))
-        print('')
+
+        print('{}. 企業名 : {}'.format(cnt, test_address_book[cnt].name))
+        print('{}. 住所 : {}'.format(cnt, test_address_book[cnt].address))
+        print()
+
+print('===================================================')
+print()
 
 address_book = JPCorporateAddressBook().createAddressBook()
 
-for cnt in range(0,10):
+for cnt in range(60,70):
 
-    print('企業名 : {}'.format(address_book[cnt].name))
-    print('住所 : {}'.format(address_book[cnt].address))
-    print('')
+    print('{}. 企業名 : {}'.format(cnt, address_book[cnt].name))
+    print('{}. 住所 : {}'.format(cnt, address_book[cnt].address))
+    print()
