11b90262bcd26c59c6840cbf7c611e052745072c
Taro Kimura <tarokimuraorg@outlook.com>
2022-10-19 14:28:45 +0900

    JPCorporateAddressBookクラスの仕様変更に対応

--- test.py
+++ test.py
@@ -10,9 +10,7 @@ with open('./csv/jigyosyo.csv', 'r', encoding='utf8') as csv_file:
     csv_data = csv.reader(csv_file, delimiter=',', doublequote=True, lineterminator='\n', quotechar='"', skipinitialspace=True)
     csv_list = list(csv_data)
 
-    #print('件数 : {}'.format(len(csv_list)))
-
-    for cnt1, cnt2 in zip(range(112,122), range(110,120)):
+    for cnt1, cnt2 in zip(range(135,145), range(132,142)):
 
         print()
         print('{} : {}'.format(cnt1, csv_list[cnt1]))
@@ -22,14 +20,16 @@ with open('./csv/jigyosyo.csv', 'r', encoding='utf8') as csv_file:
         print('{}. 住所 : {}'.format(cnt1, test_address_book[cnt1].address))
         print()
 
-        if (test_address_book[cnt1].name == address_book[cnt2].name
-            and test_address_book[cnt1].address == address_book[cnt2].address):
-
-            print('企業名と住所が一致しました。')
-
+        if (test_address_book[cnt1].name == address_book[cnt2].name):
+            print('企業名が一致しました。')
         else:
-            print('企業名と住所が一致しません。')
-        
+            print('{}. 企業名が一致しません : {}'.format(cnt2, address_book[cnt2].name))
+
+        if (test_address_book[cnt1].address == address_book[cnt2].address):
+            print('住所が一致しました。')
+        else:
+            print('{}. 住所が一致しません : {}'.format(cnt2, address_book[cnt2].address))
+            
         print()
 
         print('===================================================')
